135534daed91f7e7199531e1199296b42b9d30ca
David Hayward <david.hayward@ympt.co.uk>
2025-11-13 21:25:37 +0000

    First upload of source files.

---
+++ qmkeybox_source/rules.mk
@@ -0,0 +1,9 @@
+# Enable VIA support
+VIA_ENABLE = yes
+
+# Define the MCU (ATmega32U4 for Pro Micro)
+MCU = atmega32u4
+BOOTLOADER = caterina
+
+# Other optional settings for minimal firmware size
+BOOTLOADER_ENABLE = no
